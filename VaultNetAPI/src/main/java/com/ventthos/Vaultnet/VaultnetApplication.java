a7340b39f44c7dbda0b0a8c6f80facccbbfd5d26
Ventthos <ferra04@live.com.mx>
2025-07-02 12:16:42 -0600

    Java configuration

---
+++ VaultNetAPI/src/main/java/com/ventthos/Vaultnet/VaultnetApplication.java
@@ -0,0 +1,13 @@
+package com.ventthos.Vaultnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultnetApplication.class, args);
+	}
+
+}
---
+++ VaultNetAPI/src/main/java/com/ventthos/Vaultnet/config/DotenvInitializer.java
@@ -0,0 +1,16 @@
+package com.ventthos.Vaultnet.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
